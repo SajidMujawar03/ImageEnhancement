04ae61a9b63939e1adfa3deb24eca7a01b9e7f4a
SajidMujawar03 <sajidmmujawar9460@gmail.com>
2025-03-06 13:05:26 +0530

    upload functionality

---
+++ frontend/src/components/input/DragAndDrop.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+const DragAndDrop = () => {
+  return (
+    <>
+
+    <form action="">
+
+        <input type="file" name="input" id="input" accept="image/*"/>
+
+        <button type="submit" className='bg-blue-200 w-[200px] h-[50px]'>Click to upload</button>
+
+    </form>
+    
+    </>
+  )
+}
+
+export default DragAndDrop
